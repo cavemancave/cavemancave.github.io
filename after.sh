8639e1818d7f84c131ab693fd3f592ff7f0fcbd1
cavemancave <echooffapple@gmail.com>
2023-04-15 16:24:15 +0800

    fix after.sh

--- after.sh
+++ after.sh
@@ -3,4 +3,4 @@ cp /root/www/Caddyfile /root/caddy/;
 docker exec -w /etc/caddy caddy caddy reload
 cd /root/code/cavemancave.github.io;
 git fetch --recurse-submodules;
-git reset origin/main --hard --recurse-submodules;"
+git reset origin/main --hard --recurse-submodules;
